da603ff8f90d9070f9dad3aabd7b64531726bd60
baraaAlosaily <seelbaraa@gmail.com>
2021-06-08 01:42:40 +0300

    class02

--- horned/src/component/HornedBeast.js
+++ horned/src/component/HornedBeast.js
@@ -1,13 +1,29 @@
 import React from "react";
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+  increaseTheCounter = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
   render() {
     return (
       <main>
         <div>
           <h2>{this.props.title}</h2>
-          <img src={this.props.img} alt="" title={this.props.title} />
-          <p>{this.props.desc}</p>
+          <img
+            onClick={this.increaseTheCounter}
+            style={{ width: "25rem" }}
+            src={this.props.img}
+            alt=""
+            title={this.props.title}
+          />
+          <p>{this.props.description}</p>
+          <p>
+            <span>{"\u2728"}</span> {this.state.count}
+          </p>
         </div>
       </main>
     );
